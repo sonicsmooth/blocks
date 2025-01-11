ad4b35b60ac3729b0f5a3d75d33b103955d2d9b3
Michael Schwager <nobody@anywhere.com>
2025-01-10 23:33:07 -0800

    Fixed sizing

--- nim/frames.nim
+++ nim/frames.nim
@@ -20,9 +20,8 @@ wClass(wBlockPanel of wPanel):
   # Declare out-of-order procs
   proc onPaint(self: wBlockPanel, event: wEvent)
   proc init(self: wBlockPanel, parent: wWindow) = 
-    echo "blockPanel init"
-    wPanel(self).init(parent)
-    self.backgroundColor = parent.backgroundColor
+    wPanel(self).init(parent, style=wBorderSimple)
+    self.backgroundColor = wLightBlue
     self.doubleBuffered = true
     self.setDoubleBuffered(true)
     self.wEvent_Paint do (event: wEvent): 
@@ -30,10 +29,8 @@ wClass(wBlockPanel of wPanel):
     self.wEvent_MouseMove do (event: wEvent):
       broadcastTopLevelMessage(wBaseApp, USER_MOUSE_MOVE, event.mWparam, event.mLparam)
     self.wEvent_Size do (event: wEvent):
-      echo "blockPanel Size: ", event.getSize()
       broadcastTopLevelMessage(wBaseApp, USER_SIZE, event.mWparam, event.mLparam)
   proc onPaint(self: wBlockPanel, event: wEvent) = 
-    echo "blockPanel Paint: ", event.getSize()
     var dc = PaintDC(event.window)
     for rect in self.mRectTable.values():
       setBrush(dc, Brush(rect.brushcolor))
@@ -45,8 +42,7 @@ type wMainPanel = ref object of wPanel
 
 wClass(wMainPanel of wPanel):
   proc init(self: wMainPanel, parent: wWindow) =
-    echo "mainPanel init"
-    wPanel(self).init(parent, style=wBorderSimple)
+    wPanel(self).init(parent)
     self.mBlockPanel = BlockPanel(self)
     let
       b1  = Button(self, label = "Randomize"         )
@@ -66,66 +62,44 @@ wClass(wMainPanel of wPanel):
       b15 = Button(self, label = "Load"              )
 
     proc layout_internal() =
-      echo "mainPanel layout self.size: ", self.size
-      echo "mainPanel layout self.clientSize: ", self.clientSize
       let 
         (cszw, cszh) = self.clientSize
         bmarg = 8
-        bw = 130
-        bh = 30
+        (bw, bh) = (130, 30)
+        (lbpmarg, rbpmarg, tbpmarg, bbpmarg) = (0, 8, 0, 0)
         butts = [b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15]
-      self.mBlockPanel.position = (bw + 2*bmarg, 0)
-      echo "mainPanel setting blockPanel size"
-      self.mBlockPanel.size = (cszw - bw - 2*bmarg, cszh)
+      self.mBlockPanel.position = (bw + 2*bmarg + lbpmarg, tbpmarg)
+      self.mBlockPanel.size = (cszw - bw - 2*bmarg - lbpmarg - rbpmarg, 
+                               cszh - tbpmarg - bbpmarg)
       for i, butt in butts:
         butt.position = (bmarg, bmarg + i * bh)
         butt.size     = (bw, bh)
     layout_internal()
-    self.wEvent_Size do(event: wEvent):
-      echo "mainPanel wEvent_Size event.getSize(): ", event.getSize()
-      echo "mainPanel wEvent_Size self.size: ", self.size
-      echo "mainPanel wEvent_Size self.clientSize: ", self.clientSize
+    self.wEvent_Size do (event: wEvent):
       layout_internal()
 
 wClass(wMainFrame of wFrame):
-  proc init*(self: wMainFrame, blockSize: wSize, rectTable: RectTable) = 
-    echo "mainFrame init"
+  proc init*(self: wMainFrame, newBlockSz: wSize, rectTable: RectTable) = 
     wFrame(self).init(title="Blocks Frame")
     let
       mainPanel = MainPanel(self)
-      menuBar = MenuBar(self)
-      menuFile = Menu(menuBar, "&File")
+      menuBar   = MenuBar(self)
+      menuFile  = Menu(menuBar, "&File")
       statusBar = StatusBar(self)
-    echo "  setting"
+      otherWidth  = self.size.width  - mainPanel.mBlockPanel.clientSize.width
+      otherHeight = self.size.height - mainPanel.mBlockPanel.clientSize.height
+      newWidth  = newBlockSz.width  + otherWidth
+      newHeight = newBlockSz.height + otherHeight + 23
     mainPanel.mBlockPanel.mRectTable = rectTable
-    # echo "  mainFrame self.size.width: ", self.size.width
-    # echo "  mainFrame self.clientSize.width: ", self.clientSize.width
-    # echo "  arg blockSize.width: ", blockSize.width
-    # echo "  mainPanel.size.width: ", mainPanel.size.width
-    # echo "  mainPanel.clientSize.width: ", mainPanel.clientSize.width
-    let otherWidth = self.size.width - mainPanel.mBlockPanel.clientSize.width
-    #echo "  otherWidth: ", otherWidth
-    let newWidth = blockSize.width + otherWidth
-    #let newWidth = blockSize.width - 14
-    let otherHeight = self.size.height - 
-                      mainPanel.mBlockPanel.clientSize.height - 
-                      #menuBar.clientSize.height -
-                      statusBar.size.height
-    let newHeight = blockSize.height + otherHeight
     self.size = (newWidth, newHeight)
-    echo "  done setting"
     menuFile.append(1, "Open")
     statusBar.setStatusWidths([-2, -1, 100])
     self.center()
-    echo "  show"
     self.show()
-    echo "  done show"
 
     self.wEvent_Size do (e: wEvent):
-      echo "mainFrame Size: ", e.getSize()
       mainPanel.size = (e.size.width, e.size.height - statusBar.size.height)
     self.USER_SIZE do (e: wEvent): 
-      echo "mainFrame UserSize:", e.getSize()
       statusBar.setStatusText($e.EventParam.wSize, 1)
     self.USER_MOUSE_MOVE do (e: wEvent): 
       statusBar.setStatusText($e.mMousePos, 2)
