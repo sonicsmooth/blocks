7383f921990fa03b06a3efa4610ea7c6c40ac517
Michael Schwager <nobody@anywhere.com>
2025-01-10 23:33:07 -0800

    Fixed sizing

--- blocks.nim
+++ blocks.nim
@@ -7,7 +7,7 @@ import frames, rects
 
 when isMainModule:
   randomize()
-  let init_size = (800, 800)
+  let init_size = (800, 600)
   let rectTable = InitRects(init_size)
   let app = App()
   discard MainFrame(init_size, rectTable)
