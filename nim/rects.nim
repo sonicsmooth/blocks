ebf21a9553d6afaaeefbe47a74063a16f096ba4b
Michael Schwager <michael.schwager@omniva.com>
2025-01-18 18:52:03 -0800

    Updating block move

--- nim/rects.nim
+++ nim/rects.nim
@@ -21,12 +21,21 @@ type
   Edge* = object of RootObj
     pt0*: wPoint
     pt1*: wPoint
-  VertEdge* = object of Edge
-  HorizEdge* = object of Edge
-  TopEdge* = object of HorizEdge
-  LeftEdge* = object of VertEdge
+  VertEdge*   = object of Edge
+  HorizEdge*  = object of Edge
+  TopEdge*    = object of HorizEdge
+  LeftEdge*   = object of VertEdge
   BottomEdge* = object of HorizEdge
-  RightEdge* = object of VertEdge
+  RightEdge*  = object of VertEdge
+
+proc `[]`*(table: RectTable, idxs: seq[RectID]): seq[Rect] =
+  for idx in idxs:
+    result.add(table[idx])
+
+proc `[]`*[S](table: RectTable, idxs: array[S, Rect]): array[S, Rect] = 
+  for i,idx in idxs:
+    result[i] = table[idx]
+
 
 proc `$`*(r: Rect): string =
   result =
@@ -44,35 +53,39 @@ proc `$`*(table: RectTable): string =
   for k,v in table:
     result.add(&"{k}: {v}\n")
 
-proc ToRect*(rect: Rect): wRect =
-  (rect.pos.x, rect.pos.y, rect.size.width, rect.size.height)
+# proc toWRect*(rect: Rect): wRect =
+#   (rect.pos.x, rect.pos.y, rect.size.width, rect.size.height)
 
 proc wRect*(rect: Rect): wRect =
   (rect.pos.x, rect.pos.y, rect.size.width, rect.size.height)
 
-proc UpperLeft*(rect: Rect): wPoint =
+proc wRects*(rects: seq[Rect]): seq[wRect] =
+  for rect in rects:
+    result.add(rect.wRect)
+
+proc upperLeft*(rect: Rect): wPoint =
   rect.pos
 
-proc UpperRight*(rect: Rect): wPoint =
+proc upperRight*(rect: Rect): wPoint =
   (rect.pos.x + rect.size.width, rect.pos.y)
 
-proc LowerLeft*(rect: Rect): wPoint =
+proc lowerLeft*(rect: Rect): wPoint =
   (rect.pos.x, rect.pos.y + rect.size.height)
 
-proc LowerRight*(rect: Rect): wPoint =
+proc lowerRight*(rect: Rect): wPoint =
   (rect.pos.x + rect.size.width, rect.pos.y + rect.size.height)
 
 proc Top*(rect: Rect): TopEdge =
-  TopEdge(pt0: rect.UpperLeft, pt1: rect.UpperRight)
+  TopEdge(pt0: rect.upperLeft, pt1: rect.upperRight)
 
 proc Left*(rect: Rect): LeftEdge =
-  LeftEdge(pt0: rect.UpperLeft, pt1: rect.LowerLeft)
+  LeftEdge(pt0: rect.upperLeft, pt1: rect.lowerLeft)
 
 proc Bottom*(rect: Rect): BottomEdge =
-  BottomEdge(pt0: rect.LowerLeft, pt1: rect.LowerRight)
+  BottomEdge(pt0: rect.lowerLeft, pt1: rect.lowerRight)
 
 proc Right*(rect: Rect): RightEdge =
-  RightEdge(pt0: rect.UpperRight, pt1: rect.LowerRight)
+  RightEdge(pt0: rect.upperRight, pt1: rect.lowerRight)
 
 # Comparators assume edges are truly vertical or horizontal
 # So we only look at pt0
@@ -91,7 +104,6 @@ proc `>=`*(edge1, edge2: VertEdge): bool =
 proc `==`*(edge1, edge2: VertEdge): bool =
   edge1.pt0.x == edge2.pt0.x
 
-
 proc `<`*(edge1, edge2: HorizEdge): bool =
   edge1.pt0.y < edge2.pt0.y
 
@@ -107,19 +119,69 @@ proc `>=`*(edge1, edge2: HorizEdge): bool =
 proc `==`*(edge1, edge2: HorizEdge): bool =
   edge1.pt0.y == edge2.pt0.y
 
+proc isPointInRect(pt: wpoint, rect: Rect): bool = 
+    let lrcorner: wPoint = (rect.pos.x + rect.size.width,
+                            rect.pos.y + rect.size.height)
+    pt.x >= rect.pos.x and pt.x <= lrcorner.x and
+    pt.y >= rect.pos.y and pt.y <= lrcorner.y
 
-proc RandColor: wColor = 
-  # 00bbggrr
-  let 
-    b: int = rand(255) shl 16
-    g: int = rand(255) shl 8
-    r: int = rand(255)
-  result = wColor(b or g or r)
+proc isEdgeInRect(edge: VertEdge, rect: Rect): bool =
+  let edgeInside = (edge >= rect.Left and edge <= rect.Right)
+  let pt0Inside = isPointInRect(edge.pt0, rect)
+  let pt1Inside = isPointInRect(edge.pt1, rect)
+  let pt0Outside = edge.pt0.y < rect.Top.pt0.y
+  let pt1Outside = edge.pt1.y > rect.Bottom.pt0.y
+  (pt0Inside or pt1Inside) or 
+  (pt0Outside and pt1Outside and edgeInside)
+
+proc isEdgeInRect(edge: HorizEdge, rect: Rect): bool =
+  let edgeInside = (edge >= rect.Top and edge <= rect.Bottom)
+  let pt0Inside = isPointInRect(edge.pt0, rect)
+  let pt1Inside = isPointInRect(edge.pt1, rect)
+  let pt0Outside = edge.pt0.x < rect.Left.pt0.x
+  let pt1Outside = edge.pt1.x > rect.Right.pt0.x
+  (pt0Inside or pt1Inside) or 
+  (pt0Outside and pt1Outside and edgeInside)
+
+proc isRectInRect*(rect1, rect2: Rect): bool = 
+  # Check if any corners or edges of rect2 are within rect1
+  # Generally rect1 is moving around and rect2 is part of the db
+  isEdgeInRect(rect1.Top,    rect2) or
+  isEdgeInRect(rect1.Left,   rect2) or
+  isEdgeInRect(rect1.Bottom, rect2) or
+  isEdgeInRect(rect1.Right,  rect2)
+
+proc ptInRects*(pt: wPoint, table: RectTable): seq[RectID] = 
+  # Returns seq of Rect IDs whose rect surrounds or contacts pt
+  # Optimization? -- return after first one
+  for id, rect in table:
+      if isPointInRect(pt, rect):
+        result.add(id)
+
+proc rectInRects*(rect: Rect, table: RectTable): seq[RectID] = 
+  # Return seq of Rect IDs from table that intersect rect
+  # Return seq also includes rect
+  # Typically rect is moving around and touches objs in table
+  for id, tabRect in table:
+    if isRectInRect(rect, tabRect):
+      result.add(id)
+
+proc rectInRects*(rectId: RectID, table: RectTable): seq[RectID] = 
+  rectInRects(table[rectId], table)
+
+
 
 proc RandRect(id: RectID, screenSize: wSize): Rect = 
   let rectSize: wSize = (rand(WRANGE), rand(HRANGE))
   let rectPos: wPoint = (rand(screenSize.width  - rectSize.width  - 1),
                          rand(screenSize.height - rectSize.height - 1))
+  proc RandColor: wColor = 
+    let 
+      b: int = rand(255) shl 16
+      g: int = rand(255) shl 8
+      r: int = rand(255)
+    wColor(b or g or r) # 00bbggrr
+
   result = Rect(id: id, 
                 size: rectSize,
                 pos: rectPos,
@@ -127,20 +189,46 @@ proc RandRect(id: RectID, screenSize: wSize): Rect =
                 pencolor: RandColor(), 
                 brushcolor: RandColor())
 
-proc RandomizeRectsAll*(table: var RectTable, size: wSize, qty:int) = 
+proc randomizeRectsAll*(table: var RectTable, size: wSize, qty:int) = 
   table.clear()
   for i in 1..qty:
     table.add(RandRect($i, size))
 
-proc RandomizeRectsPos*(table: RectTable, screenSize: wSize) =
+proc randomizeRectsPos*(table: RectTable, screenSize: wSize) =
   for id, rect in table:
     rect.pos = (rand(screenSize.width  - rect.size.width  - 1),
                 rand(screenSize.height - rect.size.height - 1))
 
 # This works because Rect is a ref object
-proc MoveRectDelta(rect: Rect, delta: wPoint) =
+proc moveRectDelta(rect: Rect, delta: wPoint) =
   rect.pos = rect.pos + delta
 
-proc MoveRect*(rect: Rect, oldpos, newpos: wPoint) = 
+proc moveRect*(rect: Rect, oldpos, newpos: wPoint) = 
   let delta = newpos - oldpos
-  MoveRectDelta(rect, delta)
+  moveRectDelta(rect, delta)
+
+proc boundingBox*(rects: seq[wRect]): wRect =
+  # Bbox from a bunch of wRects
+  var left = rects[0].x
+  var top = rects[0].y
+  var right = rects[0].x + rects[0].width
+  var bottom = rects[0].y + rects[0].height
+  for rect in rects[1..rects.high]:
+    left = min(left, rect.x)
+    top = min(top, rect.y)
+    right = max(right, rect.x+rect.width)
+    bottom = max(bottom, rect.y+rect.height)
+  (x:left, y: top, width: right - left, height: bottom - top)
+
+proc boundingBox*(rects: seq[Rect]): wRect =
+  # Bbox from a bunch of Rects
+  boundingBox(wRects(rects))
+
+proc expand*(rect: wRect, amt: int): wRect =
+  (x: rect.x - amt,
+   y: rect.y - amt,
+   width: rect.width + 2*amt,
+   height: rect.height + 2*amt)
+  
+proc expand*(rect: Rect, amt: int): wRect =
+  rect.wRect.expand(amt)
--- nim/tst.nim
+++ nim/tst.nim
@@ -13,28 +13,40 @@ proc `$`(rect: ref Rect, i: int): string =
   inc(rect.x, i)
   result = fmt"[{rect.x}, {rect.y}]"
 
+
+
 var xx = (ref Rect)(x:15, y:20)
 
 type RectTable = Table[string, string]
 
+proc `[]`[T](table: T, idxs: seq[string]): seq[string] = 
+  echo table.type
+  for idx in idxs:
+    result.add(table[idx])
+
+proc `[]`[S](table: Table, idxs: array[S, string]): array[S, string] = 
+  for i,idx in idxs:
+    result[i] = table[idx]
 
 var tab:RectTable = {"hi":"cat", "bye":"horse"}.toTable()
 
+echo tab[@["hi","bye"]]
+echo tab[["hi","bye"]]
 
-var mRefRectTable: ref RectTable
-new mRefRectTable
-mRefRectTable[] = tab
+# var mRefRectTable: ref RectTable
+# new mRefRectTable
+# mRefRectTable[] = tab
+# # echo mRefRectTable[]
+# # echo type(mRefRectTable)
+# # echo type(mRefRectTable["hi"])
+
+# var rf = mRefRectTable
+# echo rf
+# rf["hi"] = "monitor"
+
 # echo mRefRectTable[]
-# echo type(mRefRectTable)
-# echo type(mRefRectTable["hi"])
+# echo rf[]
 
-var rf = mRefRectTable
-echo rf
-rf["hi"] = "monitor"
-
-echo mRefRectTable[]
-echo rf[]
-
-var bla: array[0,int]
-if bla.len == 0:
-  echo "ya"
+# var bla: array[0,int]
+# if bla.len == 0:
+#   echo "ya"
