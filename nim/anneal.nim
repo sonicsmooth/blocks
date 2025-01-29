0dd1ed910295f56a4e196b44d1fa352bdb4092ac
Michael Schwager <nobody@anywhere.com>
2025-01-28 20:16:01 -0800

    Working on anneal

--- nim/anneal.nim
+++ nim/anneal.nim
@@ -1,9 +1,10 @@
 # Simulated annealing
 import std/[random]
 import wnim
+from wnim/private/wHelper import `+`
 import rects
 
-const NUM_NEXT_STATES = 100
+const NUM_NEXT_STATES = 10
 const MAX_TEMP = 100.0
 # At each temperature generate 100 randomized next states
 # The higher the temperature, the more each block moves around
@@ -13,20 +14,27 @@ const MAX_TEMP = 100.0
 
 proc moveAmt(temp: float, screenSize: wSize): wPoint =
   # At maximum temp, maximum move is wSize/2
-  let halfWidth: float  = screenSize.width.float / 2.0
-  let halfheight: float = screenSize.height.float / 2.0
+  let halfWidth: float  = screenSize.width.float / 4.0
+  let halfheight: float = screenSize.height.float / 4.0
   let maxX: float       = temp/100.0 * halfWidth
   let maxY: float       = temp/100.0 * halfHeight
-  let xmv: int          = (rand(maxX) - halfWidth / 2).int
-  let xmy: int          = (rand(maxY) - halfHeight / 2).int
+  let xmv: int          = (rand(maxX) - maxX/2.0).int
+  let xmy: int          = (rand(maxY) - maxY/2.0).int
   result = (xmv, xmy)
 
 
+proc nextStates*(table: RectTable, temp: float, screenSize: wSize) =
+  # Take existing state and generate next states
+  let currentStateTable = positions(table)
+  type StateType = Table[RectID, wPoint]
+  var nextStates: seq[StateType]
+  for i in 1..NUM_NEXT_STATES:
+    var nextStateTable: StateType
+    for id, pos in currentStateTable:
+      nextStateTable[id] = pos + moveAmt(temp, screenSize)
+    nextStates.add(nextStateTable)
 
-
-proc randomizeRectsPos*(table: RectTable, temp: float, screenSize: wSize) =
-  # Take existing state and generate 
-  echo moveAmt(temp, screenSize)
-  # for id, rect in table:
-  #   rect.x = rand(screenSize.width  - rect.width  - 1)
-  #   rect.y = rand(screenSize.height - rect.height - 1)
+  # quick test -- use the first one
+  for id, pos in nextStates[0]:
+    table[id].x = pos.x
+    table[id].y = pos.y
