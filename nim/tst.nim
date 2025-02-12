53a7e8c396dfc8def747d62e4a61559a4dc1bb68
Michael Schwager <michael.schwager@outlook.com>
2025-02-12 15:25:24 -0800

    Lots of stuff in tst relating to threads

--- nim/tst.nim
+++ nim/tst.nim
@@ -1,29 +1,31 @@
-import wNim, winim
-import std/[random]
+import std/[random, locks, sequtils]
 from std/os import sleep
+import wNim, winim
 
-import std/locks
 
-var
-  thr: array[0..4, Thread[tuple[a,b: int]]]
-  L: Lock
 
-# proc threadFunc(interval: tuple[a,b: int]) {.thread.} =
-#   for i in interval.a..interval.b:
-#     acquire(L) # lock stdout
-#     echo i
-#     release(L)
+#const maxCnt = 100000
+var L: Lock
+var sharedCounter: int
+type 
+  wBlockPanel = ref object of wPanel
+  wMainPanel = ref object of wPanel
+    mBlockPanel: wBlockPanel
+    mButtons: array[1, wButton]
+  wMainFrame = ref object of wFrame
+    mMainPanel: wMainPanel
+  Rect = tuple[rect: wRect, color: wColor]
+  RSeq = seq[Rect]
+  ISeq = seq[int]
+  ThreadArg = tuple[sz:wSize, window: wMainPanel]
 
+var 
+  gRects: RSeq
+  gRChan: Channel[RSeq]
+  t3: Thread[ThreadArg]
+
+gRChan.open()
 initLock(L)
-
-# for i in 0..high(thr):
-#   createThread(thr[i], threadFunc, (i*10, i*10+5))
-# joinThreads(thr)
-
-#deinitLock(L)
-
-
-var gRects {.threadvar.}: seq[tuple[rect: wRect, color: wColor]]
 
 proc randColor: wColor = 
   let 
@@ -32,28 +34,58 @@ proc randColor: wColor =
     r: int = rand(255)
   wColor(b or g or r) # 00bbggrr
 
+proc randomizeRectsAll(sz: wSize, qty: int): RSeq {.gcsafe.} = 
+  for i in 1..qty:
+    let r: wRect = (rand(sz.width),
+                    rand(sz.height),
+                    rand(20..100),
+                    rand(20..100))
+    let c: wColor = randcolor()
+    result.add((r,c))
+
+
+proc worker(arg: ThreadArg) {.thread.} =
+  for i in 1..5:
+    gRChan.send(randomizeRectsAll(arg.sz, 100))
+    arg.window.refresh()
+    withLock(L):
+      echo "sent"
+    sleep(1000)
+  echo "Worker done"
+
+
 
 wClass(wBlockPanel of wPanel):
   proc onPaint(self: wBlockPanel, event: wEvent) = 
-    # Finally grab DC and do last blit
     var dc = PaintDC(event.window)
-    for r in gRects:
-      dc.setBrush(Brush(r.color))
-      dc.drawRectangle(r.rect)
+    if gRChan.peek() > 0:
+      while true:
+        dc.clear()
+        let (b, msg) = gRChan.tryRecv()
+        if b:
+          echo "available:", msg.len
+          gRects = msg
+          for r in gRects:
+            dc.setBrush(Brush(r.color))
+            dc.drawRectangle(r.rect)
+        else:
+          echo "not available"
+          break
+    else:
+        for r in gRects:
+          dc.setBrush(Brush(r.color))
+          dc.drawRectangle(r.rect)
 
 
   proc init(self: wBlockPanel, parent: wWindow) = 
     wPanel(self).init(parent, style=wBorderSimple)
     self.backgroundColor = wLightBlue
-    self.wEvent_Paint      do (event: wEvent): self.onPaint(event)
+    self.wEvent_Paint do (event: wEvent): self.onPaint(event)
 
 
 
-type wMainPanel = ref object of wPanel
-  mBlockPanel: wBlockPanel
-  mButtons: array[1, wButton]
 
-var thr2: array[0..1, Thread[tuple[self: wMainPanel, sz: wSize]]]
+
 
 wClass(wMainPanel of wPanel):
   proc Layout(self: wMainPanel) =
@@ -77,54 +109,28 @@ wClass(wMainPanel of wPanel):
     UpdateWindow(self.mBlockPanel.mHwnd)
     sleep(wait)
 
-
-  proc randomizeRectsAll(self: wMainPanel, qty: int, sz: wSize) = 
-    gRects.setLen(0)
-    for i in 1..qty:
-      let r: wRect = (rand(sz.width),
-                      rand(sz.height),
-                      rand(20..100),
-                      rand(20..100))
-      let c: wColor = randcolor()
-      gRects.add((r,c))
-
   proc onResize(self: wMainPanel) =
-    echo "Size of gRects: ", gRects.len
-    self.randomizeRectsAll(10, self.mBlockPanel.clientSize)
+    gRects = randomizeRectsAll(self.mBlockPanel.clientSize, 10)
     self.Layout()
       
-  proc worker(arg: tuple[self: wMainPanel, sz: wSize]) {.thread.} =
-    for i in 1..100:
-      echo i
-      acquire(L)
-      arg.self.randomizeRectsAll(100, arg.sz)
-      echo "Size of gRects: ", gRects.len
-      release(L)
-      #arg.self.forceRedraw(100)
-
   proc onButtonTest(self: wMainPanel) =
-    let arg = (self, self.mBlockPanel.cLientSize)
-    createThread(thr2[0], worker, arg)
-    joinThread(thr2[0])
-    self.refresh()
+    let sz = self.mBlockPanel.cLientSize
+    createThread(t3, worker, (sz, self))
+    # while t3.running() or gRChan.peek() > 0:
+    #   gRects = gRChan.recv()
+    #   self.forceRedraw(0)
+    # joinThread(t3)
 
   proc init(self: wMainPanel, parent: wWindow) =
     wPanel(self).init(parent)
-
-    # Create controls
     self.mButtons[ 0] = Button(self, label = "Start long thing"     )
-
-    # Set up stuff
     self.mBlockPanel = BlockPanel(self)
-
-    # Connect events
     self.wEvent_Size                    do (event: wEvent): self.onResize()
     self.mButtons[ 0].wEvent_Button     do (): self.onButtonTest()
 
 
 
-type wMainFrame = ref object of wFrame
-  mMainPanel: wMainPanel
+
 wClass(wMainFrame of wFrame):
   proc onResize(self: wMainFrame, event: wEvent) =
     self.mMainPanel.size = (event.size.width, event.size.height - self.mStatusBar.size.height)
@@ -143,7 +149,7 @@ wClass(wMainFrame of wFrame):
 
     # Do stuff
     self.size = (newWidth, newHeight)
-    self.mMainPanel.randomizeRectsAll(10, newBlockSz)
+    gRects = randomizeRectsAll(newBlockSz, 10)
 
     # Show!
     self.center()
@@ -151,14 +157,9 @@ wClass(wMainFrame of wFrame):
 
 
 
-
-
-  
-
 when isMainModule:
   randomize()
   let init_size = (800, 600)
   let app = App()
   discard MainFrame(init_size)
   app.mainLoop()
-  
