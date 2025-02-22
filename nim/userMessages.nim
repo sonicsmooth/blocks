839e15310722f1062da50db8ef61ff7954f6110b
Michael Schwager <nobody@anywhere.com>
2025-02-22 10:27:12 -0800

    Minor change

--- nim/userMessages.nim
+++ nim/userMessages.nim
@@ -1,8 +1,8 @@
 from winim/inc/winuser import WM_APP
 
 const
-    USER_SIZE*       = WM_APP + 1
-    USER_MOUSE_MOVE* = WM_APP + 2
-    USER_SLIDER*     = WM_APP + 3
-    USER_PAINT_DONE* = WM_APP + 4
-    USER_ALG_UPDATE* = WM_APP + 5
+  USER_MOUSE_MOVE* = WM_APP + 1
+  USER_SIZE*       = WM_APP + 2
+  USER_PAINT_DONE* = WM_APP + 3
+  USER_SLIDER*     = WM_APP + 4
+  USER_ALG_UPDATE* = WM_APP + 5
