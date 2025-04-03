0113b4a5bbc8a554e1320c887b20be954e864bab
Michael Schwager <sonic1@coherentminds.com>
2025-04-02 20:52:38 -0700

    Changed test

--- tst6.nim
+++ tst6.nim
@@ -1,5 +1,10 @@
-import std/[sets, tables]
-import wnim
+import arraymancer
 
-type
-  MyTab = Table[int, wtypes.wBitmap]
+let foo = vandermonde(arange(1,6), arange(1,6)).asType(int)
+echo foo
+
+echo foo[1..2, 3..4]
+echo foo[1..<3, 3..<5]
+echo foo[_, 3..4]
+echo foo[3.._, _]
+echo foo[_..2, _]
