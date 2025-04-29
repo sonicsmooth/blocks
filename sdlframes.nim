00ccf3bd4c52a6d13f8f7442e0a9f54fe172c108
Michael Schwager <sonic1@coherentminds.com>
2025-04-28 23:29:06 -0700

    Fixed sdlframes

--- sdlframes.nim
+++ sdlframes.nim
@@ -1,9 +1,8 @@
 import wnim
 import sdl2, sdl2/[image, ttf]
-import utils
+import utils, colors
 
-
-export sdl2, image, ttf
+export sdl2, image, ttf, colors
 
 
 type
@@ -26,7 +25,6 @@ type
   wSDLFrame = ref object of wFrame
     mPanel: wTestPanel
 
-
 proc rect(x,y,w,h: cint, color: Color, dir: Direction): Rect =
   result.x = x
   result.y = y
@@ -47,8 +45,9 @@ proc initSDL*() =
     "SDL2 TTF initialization failed"
 
 wClass(wSDLPanel of wPanel):
-  proc init*(self: wSDLPanel, parent: wWindow, style: wStyle) =
-    wPanel(self).init(parent, style=style)
+  proc init*(self: wSDLPanel, parent: wWindow) =
+    echo "wSDLPanel.init()"
+    wPanel(self).init(parent)
 
     self.sdlWindow = createWindowFrom(cast[pointer] (self.mHwnd))
     sdlFailIf self.sdlWindow.isNil: "Window could not be created"
@@ -67,27 +66,20 @@ wClass(wSDLPanel of wPanel):
       echo key & ": " & $cast[cint](value)
     echo "formatName: ", self.pixelFormatName
 
-
-
 wClass(wTestPanel of wSDLPanel):
   proc drawRect(self: wTestPanel, rect: Rect) =
     self.sdlRenderer.setDrawColor(rect.color)
     self.sdlRenderer.fillRect(cast[ptr sdl2.Rect](addr rect))
-
   proc drawRect(self: wTestPanel, rect: Rect, texture: TexturePtr) =
     let dstrect = cast[ptr sdl2.Rect](addr rect)
     self.sdlRenderer.copy(texture, nil, dstrect)
-
   proc toTexture(self: wTestPanel, rect: Rect): TexturePtr =
     let surface = createRGBSurface(0, rect.w, rect.h, 32, 
       rmask, gmask, bmask, amask)
-    surface.fillRect(nil, rect.color.toUint32())
+    surface.fillRect(nil, rect.color.toColorU32().uint32)
     result = self.sdlRenderer.createTextureFromSurface(surface)
-    #echo textureInfo(result)
-  
-    
-
   proc updateRect(self: wTestPanel, rect: ptr Rect) =
+    let step = 2
     if rect.dir.x == Right and rect.x >= self.size.width - rect.w:
       rect.dir.x = Left
     if rect.dir.x == Left and rect.x <= 0:
@@ -96,10 +88,10 @@ wClass(wTestPanel of wSDLPanel):
       rect.dir.y = Up
     if rect.dir.y == Up and rect.y <= 0:
       rect.dir.y = Down
-    if rect.dir.x == Right: rect.x += 2
-    else: rect.x -= 3
-    if rect.dir.y == Down: rect.y += 3
-    else: rect.y -= 2
+    if rect.dir.x == Right: rect.x += step
+    else: rect.x -= step
+    if rect.dir.y == Down: rect.y += step
+    else: rect.y -= step
 
   proc onPaint(self: wTestPanel, event: wEvent) =
     self.sdlRenderer.setDrawColor(r=110, g=132, b=174)
@@ -116,32 +108,31 @@ wClass(wTestPanel of wSDLPanel):
 
     self.sdlRenderer.present()
     self.refresh()
-
   proc init*(self: wTestPanel, parent: wWindow) =
+    echo "wTestPanel.init()"
     wSDLPanel(self).init(parent)
-    self.rects.add(rect( 10,  20, 100, 100, toSDLColor(colRed,     127), (Right, Down)))
-    self.rects.add(rect( 30,  40, 100, 100, toSDLColor(colGreen,   127), (Right, Down)))
-    self.rects.add(rect( 50,  60, 100, 100, toSDLColor(colBlue,    127), (Right, Down)))
-    self.rects.add(rect( 70,  80, 100, 100, toSDLColor(colCyan,    127), (Right, Down)))
-    self.rects.add(rect( 90, 100, 100, 100, toSDLColor(colMagenta, 127), (Right, Down)))
-    self.rects.add(rect(110, 120, 100, 100, toSDLColor(colYellow,  127), (Right, Down)))
+    self.rects.add(rect( 10,  20, 100, 100, toColor(colRed,     127), (Right, Down)))
+    self.rects.add(rect( 30,  40, 100, 100, toColor(colGreen,   127), (Right, Down)))
+    self.rects.add(rect( 50,  60, 100, 100, toColor(colBlue,    127), (Right, Down)))
+    self.rects.add(rect( 70,  80, 100, 100, toColor(colCyan,    127), (Right, Down)))
+    self.rects.add(rect( 90, 100, 100, 100, toColor(colMagenta, 127), (Right, Down)))
+    self.rects.add(rect(110, 120, 100, 100, toColor(colYellow,  127), (Right, Down)))
 
-    self.rects.add(rect(110, 120, 100, 100, toSDLColor(colTomato,          200), (Left, Down)))
-    self.rects.add(rect(130, 140, 100, 100, toSDLColor(colLawnGreen,       200), (Left, Down)))
-    self.rects.add(rect(150, 160, 100, 100, toSDLColor(colLightCoral,      200), (Left, Down)))
-    self.rects.add(rect(170, 180, 100, 100, toSDLColor(colRoyalBlue,       200), (Left, Down)))
-    self.rects.add(rect(190, 200, 100, 100, toSDLColor(colMaroon,          200), (Left, Down)))
-    self.rects.add(rect(210, 220, 100, 100, toSDLColor(colMediumTurquoise, 200), (Left, Down)))
+    self.rects.add(rect(110, 120, 100, 100, toColor(colTomato,          200), (Left, Down)))
+    self.rects.add(rect(130, 140, 100, 100, toColor(colLawnGreen,       200), (Left, Down)))
+    self.rects.add(rect(150, 160, 100, 100, toColor(colLightCoral,      200), (Left, Down)))
+    self.rects.add(rect(170, 180, 100, 100, toColor(colRoyalBlue,       200), (Left, Down)))
+    self.rects.add(rect(190, 200, 100, 100, toColor(colMaroon,          200), (Left, Down)))
+    self.rects.add(rect(210, 220, 100, 100, toColor(colMediumTurquoise, 200), (Left, Down)))
 
     for r in self.rects:
       self.rectTextures.add(self.toTexture(r))
 
     self.wEvent_Paint do (event: wEvent): self.onPaint(event)
 
-
-
 wClass(wSDLFrame of wFrame):
   proc init*(self: wSDLFrame, size: wSize) =
+    echo "wSDLFrame.init()"
     wFrame(self).init(title="SDL Frame", size=size)
     self.mPanel = TestPanel(self)
 
