14e5f8935f7938b81aeb739eacbe08c0a8ac67dc
Michael Schwager <sonic1@coherentminds.com>
2025-05-21 12:22:31 -0700

    Added file

---
+++ nim/tsts/tst10.nim
@@ -0,0 +1,7 @@
+
+
+import rects
+
+# Test whether pointers to rects.Rect and sdl2.Rect yield the same.
+# This is interesting because one use int and the other uses cint
+# also check for compatability between sdl2.Point and rects.Size
