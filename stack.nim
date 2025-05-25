5f2ed5f1993b9841c2877addaa3ba36e2f60733f
Michael Schwager <sonic1@coherentminds.com>
2025-05-25 00:35:57 -0700

    Fixed overlow problem by clamping int32 to cint sizes

--- stack.nim
+++ stack.nim
@@ -14,41 +14,25 @@ proc stackCompact*(table: var RectTable, dstRect: PRect, direction: CompactDir) 
   var rects = table.values.toSeq
   var accRects: seq[RectID]
   
-  echo "here 1"
   if direction.primax == X:
-    echo "here 2"
     for rect in rects:
       rect.rotate(Horizontal)
     rects.sort(horizCmp, Descending)
   else:
-    echo "here 3"
     for rect in rects:
       rect.rotate(Vertical)
-    echo "here 4"
     rects.sort(vertCmp, Descending)
 
-  echo "here 5"
   for rect in rects:
-    echo "here 6"
     rect.x = if isXAscending(direction): int32.high - WRANGE.b
              else:                       int32.low
     rect.y = if isYAscending(direction): int32.high - HRANGE.b
              else:                       int32.low
 
-  echo "here 7"
   for rect in rects:
-    echo "here 8"
     accRects.add(rect.id)
-    echo "here 9"
-    dump table
-    dump direction.primAsc
-    dump direction.secAsc
-    dump dstRect
-    dump accRects
     compact(table, direction.primax, direction.primAsc, dstRect, accRects)
-    echo "here 10"
     compact(table, direction.secax,  direction.secAsc,  dstRect, accRects)
-    echo "here 11"
     let bbox = boundingBox(table[accRects])
 
     case compoundDir(direction):
