77d250eae95efa4ee3f9cc85f9b4cdc889feafdb
Michael Schwager <nobody@anywhere.com>
2025-05-26 18:44:22 -0700

    Meager attempt to optimize block stacking algorithm

--- stack.nim
+++ stack.nim
@@ -8,28 +8,10 @@ import recttable, compact
 proc vertCmp (r1, r2: rects.Rect): int = cmp(r1.size.h, r2.size.h)
 proc horizCmp(r1, r2: rects.Rect): int = cmp(r1.size.w, r2.size.w)
 
-proc stackCompact*(table: var RectTable, dstRect: PRect, direction: CompactDir) =
-  var dstRect = dstRect
-  # Rotate, sort by vertical size, and move to opposite corner
-  var rects = table.values.toSeq
+proc stackCompactSub(table: var RectTable, rects: seq[RectID], dstRect: var PRect, direction: CompactDir) =
+  # Compact given IDs into given rect
   var accRects: seq[RectID]
-  
-  if direction.primax == X:
-    for rect in rects:
-      rect.rotate(Horizontal)
-    rects.sort(horizCmp, Descending)
-  else:
-    for rect in rects:
-      rect.rotate(Vertical)
-    rects.sort(vertCmp, Descending)
-
-  for rect in rects:
-    rect.x = if isXAscending(direction): int32.high - WRANGE.b
-             else:                       int32.low
-    rect.y = if isYAscending(direction): int32.high - HRANGE.b
-             else:                       int32.low
-
-  for rect in rects:
+  for rect in table[rects]:
     accRects.add(rect.id)
     compact(table, direction.primax, direction.primAsc, dstRect, accRects)
     compact(table, direction.secax,  direction.secAsc,  dstRect, accRects)
@@ -68,6 +50,30 @@ proc stackCompact*(table: var RectTable, dstRect: PRect, direction: CompactDir) 
       if bbox.TopEdge.y < dstRect.TopEdge.y:
         dstRect.x -= bbox.w
         accRects = @[rect.id]
+
+
+proc stackCompact*(table: var RectTable, dstRect: PRect, direction: CompactDir) =
+  var dstRect = dstRect
+  # Rotate, sort by vertical size, and move to opposite corner
+  var rects = table.values.toSeq
+  
+  if direction.primax == X:
+    for rect in rects:
+      rect.rotate(Horizontal)
+    rects.sort(horizCmp, Descending)
+  else:
+    for rect in rects:
+      rect.rotate(Vertical)
+    rects.sort(vertCmp, Descending)
+
+  for rect in rects:
+    rect.x = if isXAscending(direction): int32.high - WRANGE.b
+             else:                       int32.low
+    rect.y = if isYAscending(direction): int32.high - HRANGE.b
+             else:                       int32.low
+
+  stackCompactSub(table, rects.ids, dstRect, direction)
+
 
 
 
