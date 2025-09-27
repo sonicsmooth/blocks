7e29484ffbcaa90e28b0a67c993733d6a026629f
Michael Schwager <sonic1@coherentminds.com>
2025-09-26 20:56:56 -0700

    Got some snapping to work

--- pointmath.nim
+++ pointmath.nim
@@ -23,18 +23,20 @@ proc `*`*[TN:SomeNumber, TF: SomeFloat](a: tuple[x, y: TN], b: TF): tuple[x, y: 
 # TODO figure out how to multiply PxSize, WSize, PxPoint, WPoint, etc.
 
 when isMainModule:
-  echo (2,3) + (5,9)
-  echo (2'u16, 3'u16) + (5'u16,9'u16)
-  echo (2'u32, 3'u32) + (5'u32,9'u32)
-  echo (2'u64, 3'u64) + (5'u64,9'u64)
-  echo (-2'i64, 3'i64) + (5'i64,9'i64)
-  echo (2.0, 3.0) + (5.0, 9.0)
-
+  import rects
+  assert (2,3) + (5,9) == (7, 12)
+  assert (2'u16, 3'u16) + (5'u16,9'u16) == (7'u16, 12'u16)
+  assert (2'u32, 3'u32) + (5'u32,9'u32) == (7'u32, 12'u32)
+  assert (2'u64, 3'u64) + (5'u64,9'u64) == (7'u64, 12'u64)
+  assert (-2'i64, 3'i64) + (5'i64, 9'i64) == (3'i64, 12'i64)
+  assert (2.0, 3.0) + (5.0, 9.0) == (7.0, 12.0)
 
   var jt = (15,16)
-  echo jt
-  echo jt+jt
   jt += (1,1)
-  echo jt
+  assert jt == (16, 17)
   jt -= (1,1)
-  echo jt
+  assert jt == (15, 16)
+
+  var r = DBRect(x: 15, y: 20, w: 50, h: 30)
+  assert r.pos + (10, 20) == (25, 40)
+  echo (r.pos + (10, 20).WPoint).typeof
