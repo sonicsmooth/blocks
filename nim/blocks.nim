4af9fd1042f2f31c5e8a37e88b5aac9e2016c91f
Michael Schwager <sonic1@coherentminds.com>
2025-10-15 12:58:08 -0700

    Added options

--- nim/blocks.nim
+++ nim/blocks.nim
@@ -1,44 +1,48 @@
-import std/random
+import std/[os, parseopt, paths, random, strutils, strformat]
 import wNim/[wApp, wWindow, wUtils]
-import mainframe, sdlframes, db
+import appopts, mainframe, sdlframes, db
 import concurrent, anneal
 
-# TODO: options for text, hover, qty, bbox, dstrect, testrect
 # TODO: buttons for above
 
 
-when compileOption("profiler"):
-  echo "profiling"
-  import std/nimprof
+when isMainModule:
+  when compileOption("profiler"):
+    echo "profiling"
+    import std/nimprof
+  try:
+    gAppOpts = parseAppOptions()
+    if gAppOpts.appHelp:
+      showAppHelp(gAppOpts)
+      system.quit()
 
-try:
-  randomize()
-  wSetSystemDpiAware()
-  when defined(debug):
-    echo "DPI: ", wAppGetDpi()
-  
-  # Start stuff
-  concurrent.init()
-  anneal.init()
-  sdlframes.initSDL()
-  db.initDb()
-  
-  # Main data and window
-  let app = App()
-  let init_size = (800, 800)
-  let frame = MainFrame(init_size)
-  
-  # Go App!
-  frame.center()
-  frame.show()
-  app.mainLoop()
+    randomize()
+    wSetSystemDpiAware()
+    when defined(debug):
+      echo "DPI: ", wAppGetDpi()
 
-  # Shut down
-  concurrent.deinit()
-  anneal.deinit()
+    # Start stuff
+    concurrent.init()
+    anneal.init()
+    sdlframes.initSDL()
+    db.initDb()
     
-except Exception as e:
-  echo "Exception!"
-  echo e.msg
-  echo getStackTrace(e)
+    # Main data and window
+    let app = App()
+    let init_size = (800, 800)
+    let frame = MainFrame(init_size)
+    
+    # Go App!
+    frame.center()
+    frame.show()
+    app.mainLoop()
+
+    # Shut down
+    concurrent.deinit()
+    anneal.deinit()
+      
+  except Exception as e:
+    echo "Exception!"
+    echo e.msg
+    echo getStackTrace(e)
   
