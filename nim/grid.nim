cad5818f56e68f7d70239a3755b2e55af21a57b7
Michael Schwager <sonic1@coherentminds.com>
2025-10-30 13:54:16 -0700

    Some improvements with divisions selection

--- nim/grid.nim
+++ nim/grid.nim
@@ -1,4 +1,4 @@
-import std/[algorithm, math, sequtils, strformat]
+import std/[algorithm, enumerate, math, sequtils, strformat]
 import sdl2
 import colors
 from arange import arange
@@ -58,47 +58,33 @@ proc `majorYSpace=`*(grid: Grid, val: WType) =
     grid.mMinorYSpace = val / grid.mZctrl.base
   grid.mMajorYSpace = grid.mMinorYSpace * grid.mZctrl.base
 
-proc newGrid*(zc: ZoomCtrl): Grid
 proc allowedDivisions*(grid: Grid): seq[int8] =
   # Return list of allowable divisions,
-  # that is, which values in 2..10 divide
+  # that is, which values in 2..16 divide
   # major grid space evenly.  If the result
   # for X and Y are different, then return
   # the intersection
   var xset, yset: set[int8]
-  for d in int8(2) .. int8(10):
+  for d in int8(2) .. int8(16):
     if grid.mMajorXSpace mod d == 0: xset.incl(d)
     if grid.mMajorYSpace mod d == 0: yset.incl(d)
   (xset * yset).toSeq
+
+proc allowedDivisionsStr*(grid: Grid): seq[string] =
+  for d in grid.allowedDivisions:
+    result.add($d)
 
 proc setDivisions*(grid: Grid, val: int8): bool =
   # Change grid's zctrl's base aka divsions, and update
   # minor grid size to ensure major grid size stays the
   # same.  Return true/false if base can/cannot be set
   # exactly.
+  echo &"Set Divisions: {val}"
   result = val in grid.allowedDivisions()
   if result:
     grid.mZctrl.base = val
-    grid.mMinorXSpace = grid.mMinorXSpace div val
-    grid.mMinorYSpace = grid.mMinorYSpace div val
-
-proc divNextUp*(grid: Grid, val: int8): int8 =
-  # Return next higher allowable division from val
-  for d in grid.allowedDivisions:
-    if d > val:
-      echo &"{val} up to {d}"
-      return d
-  echo &"{val} up to 0"
-  return 0
-
-proc divNextDown*(grid: Grid, val: int8): int8 =
-  # Return next higher allowable division from val
-  for d in grid.allowedDivisions.reversed:
-    if d < val:
-      echo &"{val} down to {d}"
-      return d
-  echo &"{val} down to 0"
-  return 0
+    grid.mMinorXSpace = grid.mMajorXSpace div val
+    grid.mMinorYSpace = grid.mMajorYSpace div val
 
 proc minDelta*[T](grid: Grid, scale: Scale): tuple[x,y: T] =
   # Return minimum grid spacing
